988ce2ec92988db8acd099b3ae7ab71dc6c5e233
Ali Al Naimy <archivealifaeq@gmail.com>
2026-09-06 22:47:28 +0300

    feat: واجهة الأدمن عربية RTL متكاملة - توسيط وتنسيق إبداعي وخط Cairo وساعة مباشرة

--- config/settings.py
+++ config/settings.py
@@ -176,11 +176,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Baghdad'
 
 USE_I18N = True
+
+FORMAT_MODULE_PATH = 'config.formats'
 
 USE_TZ = True
 
